302efa81bf249fd4795bc73d22a615f540b6f08a
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-28 12:55:44 +0100

    add milestone 3
    -added milestone 3 (to adjust and semplify with functions)
    -modified something in css and html
    -removed static code for columns in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,37 @@
 //create variables to get DOM elements
-const gridRowEl = document.getElementById('grid_row');
-console.log(gridRowEl);
+const rowEl = document.getElementById('grid_row');
+console.log(rowEl);
 
 //create variable for api url to use it in ajax call
-const urlCard = "https://lanciweb.github.io/demo/api/pictures/";
+const callUrl = "https://lanciweb.github.io/demo/api/pictures/";
 
 //get data from api usingh ajax call
-fetch(urlCard)
+fetch(callUrl)
   //get response data and transform it in json object
   .then(response => response.json())
   //get json object and print it 
   .then(data => {
 
-    console.log(data);
+    //forEach cicle to get through all the elements of the array received and transformed
+    data.forEach((element) => {
 
+      //create markup to add it dinamically in the page
+      const markup = `
+        <div class="col-4">
+          <div class="image_card">
+            <img id="img_pin" src="./assets/img/pin.svg" alt="image pin">
+            <img id="img_photo" class="img-fluid" src="${element.url}"
+              alt="${element.id} image">
+            <div id="img_date">${element.date}</div>
+            <div id="img_title">${element.title.toUpperCase()}</div>
+          </div>
+        </div>
+      `;
 
-  }).catch(error => console.error(error));
+      //insert markup to the page
+      rowEl.insertAdjacentHTML('beforeend', markup);
+    });
+
+  })
+  //cacth some error eventually
+  .catch(error => console.error(error));
