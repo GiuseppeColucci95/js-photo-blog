2b4e2b0348ef8c948a4b0accff731b046a81928b
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-28 12:20:04 +0100

    add milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('works!');
+//create variable for api url
+const urlCard = "https://lanciweb.github.io/demo/api/pictures/";
+
+//get data from api usingh ajax call
+fetch(urlCard)
+  //get response data and transform it in json object
+  .then(response => response.json())
+  //get json object and print it 
+  .then(data => {
+    console.log(data);
+  })
