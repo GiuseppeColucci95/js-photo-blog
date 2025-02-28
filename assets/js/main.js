f7d067076a5aa38ce750e6fb6f2b39f582ad831c
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-28 12:35:33 +0100

    edit milestone 1
    -added pin to the cards
    -added shadows to the cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
-//create variable for api url
+//create variables to get DOM elements
+const gridRowEl = document.getElementById('grid_row');
+console.log(gridRowEl);
+
+//create variable for api url to use it in ajax call
 const urlCard = "https://lanciweb.github.io/demo/api/pictures/";
 
 //get data from api usingh ajax call
@@ -7,5 +11,8 @@ fetch(urlCard)
   .then(response => response.json())
   //get json object and print it 
   .then(data => {
+
     console.log(data);
-  })
+
+
+  }).catch(error => console.error(error));
